eb69de983cacc7d3970cda588fffc01f0e4bf27b
Allan Juhanson <allan.juhanson@nortal.com>
2016-11-17 11:46:34 +0200

    MOPP-87 using remote service for pin actions

--- app/src/main/java/ee/ria/EstEIDUtility/PinUtilitiesActivity.java
+++ app/src/main/java/ee/ria/EstEIDUtility/PinUtilitiesActivity.java
@@ -1,23 +1,28 @@
 package ee.ria.EstEIDUtility;
 
-import android.app.PendingIntent;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
-import android.nfc.NfcAdapter;
-import android.nfc.Tag;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.os.RemoteException;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import ee.ria.aidl.token.tokenaidllibrary.PinActionsListener;
+import ee.ria.aidl.token.tokenaidllibrary.TokenAidlInterface;
 
 public class PinUtilitiesActivity extends AppCompatActivity {
 
     TextView content;
-    private SMInterface sminterface = null;
-    private EstEIDToken eidToken;
-    private NfcAdapter nfcAdapter;
-    private PendingIntent pendingIntent;
+    private TokenAidlInterface service;
+    private RemoteServiceConnection serviceConnection;
+    private boolean serviceBound = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,83 +31,42 @@ public class PinUtilitiesActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-        nfcAdapter = NfcAdapter.getDefaultAdapter(this);
-        pendingIntent = PendingIntent.getActivity(this, 0, new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
         content = (TextView) findViewById(R.id.pin_util_content);
-        enableButtons(false);
+        connectService();
     }
 
     @Override
-    protected void onResume () {
-        super.onResume();
-        if (nfcAdapter != null) {
-            nfcAdapter.enableForegroundDispatch(this, pendingIntent, null, null);
-        }
+    protected void onStart() {
+        super.onStart();
+        connectService();
     }
 
     @Override
-    protected void onPause() {
-        super.onPause();
-        if (nfcAdapter != null) {
-            nfcAdapter.disableForegroundDispatch(this);
+    protected void onStop() {
+        super.onStop();
+        if (serviceBound) {
+            unbindService(serviceConnection);
+            serviceBound = false;
         }
     }
 
-    @Override
-    public void onNewIntent(Intent intent) {
-        super.onNewIntent(intent);
-        setIntent(intent);
-        if (nfcAdapter == null) {
-            return;
-        }
-        sminterface = new SMInterface.NFC((Tag) intent.getParcelableExtra(NfcAdapter.EXTRA_TAG));
-        sminterface.connect(new SMInterface.Connected() {
-            @Override
-            public void connected() {
-                enableButtons(sminterface != null);
-            }
-        });
-        eidToken = new EstEIDToken(sminterface, this);
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        if (sminterface != null) {
-            sminterface.close();
-        }
-    }
-
-    public void connectReader(View view) {
-        sminterface = SMInterface.getInstance(this, SMInterface.ACS);
-        if (sminterface == null) {
-            content.setText("No readers connected");
-            return;
-        }
-        sminterface.connect(new SMInterface.Connected() {
-            @Override
-            public void connected() {
-                enableButtons(sminterface != null);
-            }
-        });
-        eidToken = new EstEIDToken(sminterface, this);
+    private void connectService() {
+        serviceConnection = new RemoteServiceConnection();
+        Intent i = new Intent("ee.ria.aidl.token.tokenaidlservice.TokenService");
+        i.setPackage("ee.ria.aidl.token.tokenaidlservice");
+        bindService(i, serviceConnection, Context.BIND_AUTO_CREATE);
     }
 
     public void changePin(View view) {
         EditText currentPinPuk = (EditText)findViewById(R.id.pin_util_current);
         EditText newPinPuk = (EditText)findViewById(R.id.pin_util_new);
         try {
-            Token.PinType type = null;
+            Token.PinType type = getPinType(view.getId());
             switch (view.getId()) {
-                case R.id.pin_util_change1: type = Token.PinType.PIN1; break;
-                case R.id.pin_util_change2: type = Token.PinType.PIN2; break;
-                case R.id.pin_util_change_puk: type = Token.PinType.PUK; break;
+                case R.id.pin_util_change1: service.changePin1(new ServiceCallbackStub(type), currentPinPuk.getText().toString(), newPinPuk.getText().toString()); break;
+                case R.id.pin_util_change2: service.changePin2(new ServiceCallbackStub(type), currentPinPuk.getText().toString(), newPinPuk.getText().toString()); break;
+                case R.id.pin_util_change_puk: service.changePuk(new ServiceCallbackStub(type), currentPinPuk.getText().toString(), newPinPuk.getText().toString()); break;
             }
-            boolean status = eidToken.changePin(currentPinPuk.getText().toString().getBytes(),
-                    newPinPuk.getText().toString().getBytes(),
-                    type);
-            content.setText(status ? type.name() + " change success" : type.name() + " change failed");
         } catch (Exception e) {
             e.printStackTrace();
             content.setText(e.getMessage());
@@ -112,17 +76,26 @@ public class PinUtilitiesActivity extends AppCompatActivity {
     public void unblockPin(View view) {
         EditText currentPinPuk = (EditText)findViewById(R.id.pin_util_current);
         try {
-            Token.PinType type = null;
+            Token.PinType type = getPinType(view.getId());
             switch (view.getId()) {
-                case R.id.pin_util_unblock1: type = Token.PinType.PIN1; break;
-                case R.id.pin_util_unblock2: type = Token.PinType.PIN2; break;
+                case R.id.pin_util_unblock1: service.unblockPin1(new ServiceCallbackStub(type), currentPinPuk.getText().toString()); break;
+                case R.id.pin_util_unblock2: service.unblockPin2(new ServiceCallbackStub(type), currentPinPuk.getText().toString()); break;
             }
-            boolean status = eidToken.unblockPin(currentPinPuk.getText().toString().getBytes(), type);
-            content.setText(status ? type.name() + " unblock success" : type.name() + " unblock failed");
         } catch (Exception e) {
             e.printStackTrace();
             content.setText(e.getMessage());
         }
+    }
+
+    private Token.PinType getPinType(int id) {
+        switch (id) {
+            case R.id.pin_util_change1: return Token.PinType.PIN1;
+            case R.id.pin_util_change2: return Token.PinType.PIN2;
+            case R.id.pin_util_unblock1: return Token.PinType.PIN1;
+            case R.id.pin_util_unblock2: return Token.PinType.PIN2;
+            case R.id.pin_util_change_puk: return Token.PinType.PUK;
+        }
+        return null;
     }
 
     public void enableButtons( boolean enable) {
@@ -133,6 +106,45 @@ public class PinUtilitiesActivity extends AppCompatActivity {
         findViewById(R.id.pin_util_change_puk).setEnabled(enable);
         findViewById(R.id.pin_util_unblock1).setEnabled(enable);
         findViewById(R.id.pin_util_unblock2).setEnabled(enable);
+    }
+
+    class RemoteServiceConnection implements ServiceConnection {
+        public void onServiceConnected(ComponentName name, IBinder boundService) {
+            service = TokenAidlInterface.Stub.asInterface((IBinder) boundService);
+            serviceBound = true;
+            enableButtons(true);
+            Toast.makeText(PinUtilitiesActivity.this, "Service connected", Toast.LENGTH_LONG)
+                    .show();
+        }
+
+        public void onServiceDisconnected(ComponentName name) {
+            service = null;
+            serviceBound = false;
+            enableButtons(false);
+            Toast.makeText(PinUtilitiesActivity.this, "Service disconnected", Toast.LENGTH_LONG)
+                    .show();
+        }
+
+    }
+
+    private class ServiceCallbackStub extends PinActionsListener.Stub {
+
+        private Token.PinType pinType;
+
+        public ServiceCallbackStub(Token.PinType pinType) {
+            super();
+            this.pinType = pinType;
+        }
+
+        @Override
+        public void onPinActionSuccessful() throws RemoteException {
+            content.setText(pinType.name() + " change success");
+        }
+
+        @Override
+        public void onPinActionFailed(String reason) throws RemoteException {
+            content.setText(pinType.name() + " change failed" + "reason: " + reason);
+        }
     }
 
 }
