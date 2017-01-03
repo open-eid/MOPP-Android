002e68f3bc8d40d80634db49d7811746952a76c8
Allan Juhanson <allan.juhanson@nortal.com>
2017-01-03 12:36:47 +0200

    [MOPP-43] splash screen

---
+++ app/src/main/java/ee/ria/EstEIDUtility/activity/SplashActivity.java
@@ -0,0 +1,26 @@
+package ee.ria.EstEIDUtility.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static int SPLASH_TIME_OUT = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, DashboardMenuActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+
+}
