24f7e569f5f8c4da44ce8aef3eb934c0cc0b5d48
Tõnis Telga <telgat@gmail.com>
2017-04-04 13:18:59 +0300

    Change the app name in mID request to RIA DigiDoc

--- app/src/main/java/ee/ria/DigiDoc/mid/CreateSignatureRequestBuilder.java
+++ app/src/main/java/ee/ria/DigiDoc/mid/CreateSignatureRequestBuilder.java
@@ -40,7 +40,7 @@ public class CreateSignatureRequestBuilder {
     private static final String FORMAT = "BDOC";
     private static final String VERSION = "2.1";
     private static final String MESSAGING_MODE = "asynchClientServer";
-    private static final String SERVICE_NAME = "DigiDoc3";
+    private static final String SERVICE_NAME = "RIA DigiDoc";
     private static final int ASYNC_CONFIGURATION = 0;
     private static final String DIGEST_TYPE = "sha256";
     private static final String DIGEST_METHOD = "http://www.w3.org/2001/04/xmlenc#sha256";
