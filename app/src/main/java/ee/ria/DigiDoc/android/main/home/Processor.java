7855b34a236e92e6d003f3628e3ba54dd96c1f00
Aleks Krause <aleks.krause@fob-solutions.com>
2018-04-18 14:16:10 +0300

    Help menu button now opens browser and url in default language. Removed help screen.

--- app/src/main/java/ee/ria/DigiDoc/android/main/home/Processor.java
+++ app/src/main/java/ee/ria/DigiDoc/android/main/home/Processor.java
@@ -1,5 +1,7 @@
 package ee.ria.DigiDoc.android.main.home;
 
+import android.app.Application;
+import android.net.Uri;
 import android.support.annotation.IdRes;
 
 import javax.inject.Inject;
@@ -8,7 +10,6 @@ import ee.ria.DigiDoc.R;
 import ee.ria.DigiDoc.android.crypto.CryptoHomeScreen;
 import ee.ria.DigiDoc.android.eid.EIDHomeScreen;
 import ee.ria.DigiDoc.android.main.about.AboutScreen;
-import ee.ria.DigiDoc.android.main.help.HelpScreen;
 import ee.ria.DigiDoc.android.main.settings.SettingsScreen;
 import ee.ria.DigiDoc.android.signature.home.SignatureHomeScreen;
 import ee.ria.DigiDoc.android.signature.list.SignatureListScreen;
@@ -18,7 +19,7 @@ import ee.ria.DigiDoc.android.utils.navigator.Transaction;
 import io.reactivex.Observable;
 import io.reactivex.ObservableSource;
 import io.reactivex.ObservableTransformer;
-
+import android.content.Intent;
 final class Processor implements ObservableTransformer<Action, Result> {
 
     private final ObservableTransformer<Action.NavigationAction, Result.NavigationResult>
@@ -26,7 +27,7 @@ final class Processor implements ObservableTransformer<Action, Result> {
 
     private final ObservableTransformer<Action.MenuAction, Result.MenuResult> menu;
 
-    @Inject Processor(Navigator navigator) {
+    @Inject Processor(Application application, Navigator navigator) {
         navigation = upstream -> upstream.switchMap(action ->
                 Observable.just(Result.NavigationResult
                         .create(navigationItemToScreen(action.item()))));
@@ -37,7 +38,13 @@ final class Processor implements ObservableTransformer<Action, Result> {
             if (isOpen != null) {
                 return Observable.just(Result.MenuResult.create(isOpen));
             } else if (item != null) {
-                navigator.execute(Transaction.push(menuItemToScreen(item)));
+                if(item == R.id.mainHomeMenuHelp){
+                    navigator.execute(Transaction.
+                            activity(new Intent(Intent.ACTION_VIEW, Uri.parse(application.getString(R.string.help_url))),
+                            null));
+                } else {
+                    navigator.execute(Transaction.push(menuItemToScreen(item)));
+                }
                 return Observable.just(Result.MenuResult.create(false));
             }
             throw new IllegalStateException("Action is in invalid state: " + action);
@@ -67,8 +74,6 @@ final class Processor implements ObservableTransformer<Action, Result> {
 
     private Screen menuItemToScreen(@IdRes int item) {
         switch (item) {
-            case R.id.mainHomeMenuHelp:
-                return HelpScreen.create();
             case R.id.mainHomeMenuRecent:
                 return SignatureListScreen.create();
             case R.id.mainHomeMenuSettings:
