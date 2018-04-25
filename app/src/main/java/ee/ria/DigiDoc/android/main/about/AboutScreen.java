05af3ea63012c4d0e39f71e33ed0b4d9482e6085
aleks5751 <aleks5751@hotmail.com>
2018-04-25 15:28:20 +0300

    About screen (#6)

--- app/src/main/java/ee/ria/DigiDoc/android/main/about/AboutScreen.java
+++ app/src/main/java/ee/ria/DigiDoc/android/main/about/AboutScreen.java
@@ -21,9 +21,6 @@ public final class AboutScreen extends ConductorScreen {
 
     @Override
     protected View view(Context context) {
-        TextView view = new TextView(context);
-        view.setText(R.string.main_home_menu_about);
-        view.setGravity(Gravity.CENTER);
-        return view;
+        return new AboutView(context);
     }
 }
