a860e4131e5a7e37bca72cd8c91580eabe7ad281
Miko Väli <mikovali@gmail.com>
2018-05-04 16:30:19 +0300

    Included libdigidocpp in about screen

--- app/src/main/java/ee/ria/DigiDoc/android/main/about/AboutView.java
+++ app/src/main/java/ee/ria/DigiDoc/android/main/about/AboutView.java
@@ -11,6 +11,7 @@ import ee.ria.DigiDoc.android.Application;
 import ee.ria.DigiDoc.android.utils.ViewDisposables;
 import ee.ria.DigiDoc.android.utils.navigator.Navigator;
 import ee.ria.DigiDoc.android.utils.navigator.Transaction;
+import ee.ria.libdigidocpp.digidoc;
 
 import static com.jakewharton.rxbinding2.support.v7.widget.RxToolbar.navigationClicks;
 
@@ -22,6 +23,7 @@ public final class AboutView extends CoordinatorLayout {
 
     private final ViewDisposables disposables;
 
+    private TextView libDigidocppVersionTitle;
     private TextView riaDigiDocVersionTitle;
     private TextView preferenceV7FixVersionTitle;
     private TextView guavaVersionTitle;
@@ -52,6 +54,7 @@ public final class AboutView extends CoordinatorLayout {
         navigator = Application.component(context).navigator();
         disposables = new ViewDisposables();
 
+        libDigidocppVersionTitle = findViewById(R.id.mainAboutLibdigidocppVersionTitle);
         riaDigiDocVersionTitle = findViewById(R.id.mainAboutRiaDigiDocVersionTitle);
         preferenceV7FixVersionTitle =
                 findViewById(R.id.mainAboutPreferenceV7FixVersionTitle);
@@ -97,27 +100,53 @@ public final class AboutView extends CoordinatorLayout {
     }
 
     private void appendTitleVersions() {
-        riaDigiDocVersionTitle.append(" Version " + BuildConfig.VERSION_NAME);
-        preferenceV7FixVersionTitle.append(" " + BuildConfig.PREFERENCE_V7_FIX_VERSION);
-        guavaVersionTitle.append(" " + BuildConfig.GUAVA_VERSION);
-        commonsIoVersionTitle.append(" " + BuildConfig.COMMONS_IO_VERSION);
-        okioVersionTitle.append(" " + BuildConfig.OKIO_VERSION);
-        timberVersionTitle.append(" " + BuildConfig.TIMBER_VERSION);
-        butterknifeVersionTitle.append(" " + BuildConfig.BUTTERKNIFE_VERSION);
-        okHttpVersionTitle.append(" " + BuildConfig.OK_HTTP_VERSION);
-        retrofitVersionTitle.append(" " + BuildConfig.RETROFIT_VERSION);
-        spongyCastleVersionTitle.append(" " + BuildConfig.SPONGY_CASTLE_VERSION);
-        materialValuesVersionTitle.append(" " + BuildConfig.MATERIAL_VALUES_VERSION);
-        daggerVersionTitle.append(" " + BuildConfig.DAGGER_VERSION);
-        conductorVersionTitle.append(" " + BuildConfig.CONDUCTOR_VERSION);
-        rxJavaVersionTitle.append(" " + BuildConfig.RX_JAVA_VERSION);
-        rxAndroidVersionTitle.append(" " + BuildConfig.RX_ANDROID_VERSION);
-        rxBindingVersionTitle.append(" " + BuildConfig.RX_BINDER_VERSION);
-        autoValueVersionTitle.append(" " + BuildConfig.AUTO_VALUE_VERSION);
-        autoValueParcelVersionTitle.append(" " + BuildConfig.AUTO_VALUE_PARCEL_VERSION);
-        threeTenAbpVersionTitle.append(" " + BuildConfig.THREE_TEN_ABP_VERSION);
-        expandableLayoutVersionTitle.append(" " + BuildConfig.EXPANDABLE_LAYOUT_VERSION);
-        junitVersionTitle.append(" " + BuildConfig.JUNIT_VERSION);
-        truthVersionTitle.append(" " + BuildConfig.TRUTH_VERSION);
+        riaDigiDocVersionTitle.setText(getResources().getString(
+                R.string.main_about_ria_digidoc_version_title, BuildConfig.VERSION_NAME));
+        libDigidocppVersionTitle.setText(getResources().getString(
+                R.string.main_about_libdigidocpp_title, digidoc.version()));
+        preferenceV7FixVersionTitle.setText(getResources().getString(
+                R.string.main_about_support_preference_v7_fix_title,
+                BuildConfig.PREFERENCE_V7_FIX_VERSION));
+        guavaVersionTitle.setText(getResources().getString(
+                R.string.main_about_guava_title, BuildConfig.GUAVA_VERSION));
+        commonsIoVersionTitle.setText(getResources().getString(
+                R.string.main_about_apache_commons_io_title, BuildConfig.COMMONS_IO_VERSION));
+        okioVersionTitle.setText(getResources().getString(
+                R.string.main_about_square_okio_title, BuildConfig.OKIO_VERSION));
+        timberVersionTitle.setText(getResources().getString(
+                R.string.main_about_timber_title, BuildConfig.TIMBER_VERSION));
+        butterknifeVersionTitle.setText(getResources().getString(
+                R.string.main_about_butterknife_title, BuildConfig.BUTTERKNIFE_VERSION));
+        okHttpVersionTitle.setText(getResources().getString(
+                R.string.main_about_square_okhttp_title, BuildConfig.OK_HTTP_VERSION));
+        retrofitVersionTitle.setText(getResources().getString(
+                R.string.main_about_retrofit_title, BuildConfig.RETROFIT_VERSION));
+        spongyCastleVersionTitle.setText(getResources().getString(
+                R.string.main_about_spongy_castle_title, BuildConfig.SPONGY_CASTLE_VERSION));
+        materialValuesVersionTitle.setText(getResources().getString(
+                R.string.main_about_material_values_title, BuildConfig.MATERIAL_VALUES_VERSION));
+        daggerVersionTitle.setText(getResources().getString(
+                R.string.main_about_dagger_title, BuildConfig.DAGGER_VERSION));
+        conductorVersionTitle.setText(getResources().getString(
+                R.string.main_about_conductor_title, BuildConfig.CONDUCTOR_VERSION));
+        rxJavaVersionTitle.setText(getResources().getString(
+                R.string.main_about_rxjava_title, BuildConfig.RX_JAVA_VERSION));
+        rxAndroidVersionTitle.setText(getResources().getString(
+                R.string.main_about_rxandroid_title, BuildConfig.RX_ANDROID_VERSION));
+        rxBindingVersionTitle.setText(getResources().getString(
+                R.string.main_about_rxbinding_title, BuildConfig.RX_BINDER_VERSION));
+        autoValueVersionTitle.setText(getResources().getString(
+                R.string.main_about_autovalue_title, BuildConfig.AUTO_VALUE_VERSION));
+        autoValueParcelVersionTitle.setText(getResources().getString(
+                R.string.main_about_autovalue_parcel_title, BuildConfig.AUTO_VALUE_PARCEL_VERSION));
+        threeTenAbpVersionTitle.setText(getResources().getString(
+                R.string.main_about_threetenabp_title, BuildConfig.THREE_TEN_ABP_VERSION));
+        expandableLayoutVersionTitle.setText(getResources().getString(
+                R.string.main_about_expandable_layout_title,
+                BuildConfig.EXPANDABLE_LAYOUT_VERSION));
+        junitVersionTitle.setText(getResources().getString(
+                R.string.main_about_junit_title, BuildConfig.JUNIT_VERSION));
+        truthVersionTitle.setText(getResources().getString(
+                R.string.main_about_truth_title, BuildConfig.TRUTH_VERSION));
     }
 }
