079405ef507adf37d56c94fd9701eb03202d2a91
Miko Väli <mikovali@gmail.com>
2018-10-30 12:47:13 +0200

    Change codes

--- id-card-lib/src/main/java/ee/ria/DigiDoc/idcard/ID1.java
+++ id-card-lib/src/main/java/ee/ria/DigiDoc/idcard/ID1.java
@@ -4,6 +4,7 @@ import android.util.Pair;
 import android.util.SparseArray;
 
 import com.google.common.base.Charsets;
+import com.google.common.primitives.Bytes;
 
 import org.threeten.bp.LocalDate;
 import org.threeten.bp.format.DateTimeFormatter;
@@ -11,6 +12,7 @@ import org.threeten.bp.format.DateTimeFormatterBuilder;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -19,7 +21,7 @@ import ee.ria.DigiDoc.smartcardreader.SmartCardReader;
 import ee.ria.DigiDoc.smartcardreader.SmartCardReaderException;
 import timber.log.Timber;
 
-class ID1 extends EstEIDv3d5 {
+class ID1 implements Token {
 
     private static final DateTimeFormatter DATE_FORMAT = new DateTimeFormatterBuilder()
             .appendPattern("dd MM yyyy")
@@ -28,7 +30,6 @@ class ID1 extends EstEIDv3d5 {
     private final SmartCardReader reader;
 
     ID1(SmartCardReader reader) {
-        super(reader);
         this.reader = reader;
     }
 
@@ -108,12 +109,67 @@ class ID1 extends EstEIDv3d5 {
         PIN_MAP.put(CodeType.PUK, (byte) 0x02);
     }
 
+    private static final Map<CodeType, Byte> VERIFY_PIN_MAP = new HashMap<>();
+    static {
+        VERIFY_PIN_MAP.put(CodeType.PIN1, (byte) 0x01);
+        VERIFY_PIN_MAP.put(CodeType.PIN2, (byte) 0x85);
+        VERIFY_PIN_MAP.put(CodeType.PUK, (byte) 0x02);
+    }
+
     @Override
     public int codeRetryCounter(CodeType type) throws SmartCardReaderException {
-        reader.transmit(0x00, 0xA4, 0x04, 0x00, new byte[] {(byte) 0xA0, 0x00, 0x00, 0x00, 0x77, 0x01, 0x08, 0x00, 0x07, 0x00, 0x00, (byte) 0xFE, 0x00, 0x00, 0x01, 0x00}, null);
         if (type.equals(CodeType.PIN2)) {
             reader.transmit(0x00, 0xA4, 0x04, 0x0C, new byte[] {0x51, 0x53, 0x43, 0x44, 0x20, 0x41, 0x70, 0x70, 0x6C, 0x69, 0x63, 0x61, 0x74, 0x69, 0x6F, 0x6E}, null);
+        } else {
+            reader.transmit(0x00, 0xA4, 0x04, 0x00, new byte[] {(byte) 0xA0, 0x00, 0x00, 0x00, 0x77, 0x01, 0x08, 0x00, 0x07, 0x00, 0x00, (byte) 0xFE, 0x00, 0x00, 0x01, 0x00}, null);
         }
         return reader.transmit(0x00, 0xCB, 0x3F, 0xFF, new byte[] {0x4D, 0x08, 0x70, 0x06, (byte) 0xBF, (byte) 0x81, PIN_MAP.get(type), 0x02, (byte) 0xA0, (byte) 0x80}, 0x00)[13];
+    }
+
+    @Override
+    public void changeCode(CodeType type, byte[] currentCode, byte[] newCode) throws SmartCardReaderException {
+        verifyPin(type, currentCode);
+        if (type.equals(CodeType.PIN2)) {
+            reader.transmit(0x00, 0xA4, 0x04, 0x0C, new byte[] {0x51, 0x53, 0x43, 0x44, 0x20, 0x41, 0x70, 0x70, 0x6C, 0x69, 0x63, 0x61, 0x74, 0x69, 0x6F, 0x6E}, null);
+        } else {
+            reader.transmit(0x00, 0xA4, 0x04, 0x00, new byte[] {(byte) 0xA0, 0x00, 0x00, 0x00, 0x77, 0x01, 0x08, 0x00, 0x07, 0x00, 0x00, (byte) 0xFE, 0x00, 0x00, 0x01, 0x00}, null);
+        }
+        reader.transmit(0x00, 0x24, 0x00, VERIFY_PIN_MAP.get(type), Bytes.concat(code(currentCode), code(newCode)), null);
+    }
+
+    @Override
+    public void unblockAndChangeCode(byte[] pukCode, CodeType type, byte[] newCode) throws SmartCardReaderException {
+    }
+
+    @Override
+    public byte[] calculateSignature(byte[] pin2, byte[] hash, boolean ecc) throws SmartCardReaderException {
+        return new byte[0];
+    }
+
+    @Override
+    public byte[] decrypt(byte[] pin1, byte[] data, boolean ecc) throws SmartCardReaderException {
+        return new byte[0];
+    }
+
+    private void verifyPin(CodeType type, byte[] code) throws SmartCardReaderException {
+        if (type.equals(CodeType.PIN2)) {
+            reader.transmit(0x00, 0xA4, 0x04, 0x0C, new byte[] {0x51, 0x53, 0x43, 0x44, 0x20, 0x41, 0x70, 0x70, 0x6C, 0x69, 0x63, 0x61, 0x74, 0x69, 0x6F, 0x6E}, null);
+        } else {
+            reader.transmit(0x00, 0xA4, 0x04, 0x00, new byte[] {(byte) 0xA0, 0x00, 0x00, 0x00, 0x77, 0x01, 0x08, 0x00, 0x07, 0x00, 0x00, (byte) 0xFE, 0x00, 0x00, 0x01, 0x00}, null);
+        }
+        try {
+            reader.transmit(0x00, 0x20, 0x00, VERIFY_PIN_MAP.get(type), code(code), null);
+        } catch (ApduResponseException e) {
+            if (e.sw1 == 0x63 || (e.sw1 == 0x69 && e.sw2 == (byte) 0x83)) {
+                throw new CodeVerificationException(type);
+            }
+            throw e;
+        }
+    }
+
+    private static byte[] code(byte[] code) {
+        byte[] padded = Arrays.copyOf(code, 12);
+        Arrays.fill(padded, code.length, padded.length, (byte) 0xFF);
+        return padded;
     }
 }
