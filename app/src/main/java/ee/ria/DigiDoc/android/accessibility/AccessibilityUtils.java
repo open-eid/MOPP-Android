c0a74f72c109c9c8a5ea27e0430e6fda747bd1b3
Jorgen Heinsoo <Jorgen.Heinsoo@nortal.com>
2019-09-04 08:41:55 +0300

    MOPPAND-482 - Accessibility util classes

---
+++ app/src/main/java/ee/ria/DigiDoc/android/accessibility/AccessibilityUtils.java
@@ -0,0 +1,81 @@
+package ee.ria.DigiDoc.android.accessibility;
+
+import android.content.Context;
+import android.os.Build;
+import android.support.annotation.StringRes;
+import android.support.v4.view.AccessibilityDelegateCompat;
+import android.support.v4.view.ViewCompat;
+import android.support.v4.view.accessibility.AccessibilityNodeInfoCompat;
+import android.view.View;
+import android.view.accessibility.AccessibilityEvent;
+import android.view.accessibility.AccessibilityManager;
+
+public class AccessibilityUtils {
+
+    public static void sendAccessibilityEvent(Context context, int eventType, @StringRes int messageResId) {
+        sendAccessibilityEvent(context, eventType, context.getString(messageResId));
+    }
+
+    public static void sendAccessibilityEvent(Context context, int eventType, CharSequence message) {
+        AccessibilityManager accessibilityManager = (AccessibilityManager) context.getSystemService(Context.ACCESSIBILITY_SERVICE);
+        if (accessibilityManager.isEnabled()) {
+            AccessibilityEvent event = AccessibilityEvent.obtain();
+            event.setEventType(eventType);
+            event.getText().add(message);
+            accessibilityManager.sendAccessibilityEvent(event);
+        }
+    }
+
+    public static void sendAccessibilityEvent(Context context, int eventType, CharSequence... messages) {
+        AccessibilityManager accessibilityManager = (AccessibilityManager) context.getSystemService(Context.ACCESSIBILITY_SERVICE);
+        if (accessibilityManager.isEnabled()) {
+            AccessibilityEvent event = AccessibilityEvent.obtain();
+            event.setEventType(eventType);
+            event.getText().add(combineMessages(messages));
+            accessibilityManager.sendAccessibilityEvent(event);
+        }
+    }
+
+    public static void sendDelayedAccessibilityEvent(Context context, int eventType, CharSequence message) {
+        AccessibilityManager accessibilityManager = (AccessibilityManager) context.getSystemService(Context.ACCESSIBILITY_SERVICE);
+        if (accessibilityManager.isEnabled()) {
+            AccessibilityEvent event = AccessibilityEvent.obtain();
+            event.setEventType(eventType);
+            event.getText().add(message);
+            accessibilityManager.sendAccessibilityEvent(event);
+        }
+    }
+
+    // TODO: lisada lõppu PANE?
+    public static void setAccessibilityPaneTitle(View view, @StringRes int titleResId) {
+        setAccessibilityPaneTitle(view, view.getResources().getString(titleResId));
+    }
+
+    public static void setAccessibilityPaneTitle(View view, String title) {
+        if (Build.VERSION.SDK_INT >= 28) {
+            view.setAccessibilityPaneTitle(title);
+        }
+    }
+
+    public static void disableContentDescription(View view) {
+        view.setContentDescription(null);
+    }
+
+    public static void disableDoubleTapToActivateFeedback(View view) {
+        ViewCompat.setAccessibilityDelegate(view, new AccessibilityDelegateCompat() {
+            @Override
+            public void onInitializeAccessibilityNodeInfo(View host, AccessibilityNodeInfoCompat info) {
+                super.onInitializeAccessibilityNodeInfo(host, info);
+                info.addAction(AccessibilityNodeInfoCompat.ACTION_FOCUS);
+            }
+        });
+    }
+
+    private static String combineMessages(CharSequence... messages) {
+        StringBuilder combinedMessage = new StringBuilder();
+        for (CharSequence message : messages) {
+            combinedMessage.append(message).append(",");
+        }
+        return combinedMessage.toString();
+    }
+}
---
+++ app/src/main/java/ee/ria/DigiDoc/android/accessibility/EventFeedBackDelegate.java
@@ -0,0 +1,42 @@
+package ee.ria.DigiDoc.android.accessibility;
+
+import android.support.annotation.StringRes;
+import android.view.View;
+import android.view.accessibility.AccessibilityEvent;
+
+public class EventFeedBackDelegate extends View.AccessibilityDelegate {
+
+    private int messageResId;
+    private boolean feedbackEventSent;
+
+    public EventFeedBackDelegate(@StringRes int messageResId) {
+        this.messageResId = messageResId;
+    }
+
+    @Override
+    public void onPopulateAccessibilityEvent(View host, AccessibilityEvent event) {
+        if (event.getEventType() == AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED) {
+            String message = host.getResources().getString(messageResId);
+            event.getText().add(message);
+            super.onPopulateAccessibilityEvent(host, event);
+        }
+
+        if (!feedbackEventSent) {
+            super.onPopulateAccessibilityEvent(host, event);
+        }
+    }
+
+    @Override
+        public void sendAccessibilityEvent(View host, int eventType) {
+        if (eventType == AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED) {
+            feedbackEventSent = true;
+            super.sendAccessibilityEvent(host, eventType);
+        }
+
+        // To prevent situation when event is fired after feedback event sending and
+        // due to that feedback event message is ignored.
+        if (!feedbackEventSent) {
+            super.sendAccessibilityEvent(host, eventType);
+        }
+    }
+}
