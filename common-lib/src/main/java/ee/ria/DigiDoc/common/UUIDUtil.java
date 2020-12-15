52ef6e4a0ef15b2eb321b56e26028596673832b5
Andres Voll <andres.voll@nortal.com>
2020-12-15 02:02:38 +0200

    Dealing with Java regex... duh
    MOPPAND-661\n\nSigned-off-by: Andres Voll <andres.voll@nortal.com>\n

--- common-lib/src/main/java/ee/ria/DigiDoc/common/UUIDUtil.java
+++ common-lib/src/main/java/ee/ria/DigiDoc/common/UUIDUtil.java
@@ -2,7 +2,7 @@ package ee.ria.DigiDoc.common;
 
 public class UUIDUtil {
 
-    private static final String UUID_REGEX = "/^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/";
+    private static final String UUID_REGEX = "^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$";
 
     private UUIDUtil() {
     }
