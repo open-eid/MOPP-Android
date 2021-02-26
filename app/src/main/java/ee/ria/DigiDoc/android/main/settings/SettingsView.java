80cc6b5d86df0e298f975b0fb07b7427204ccde7
Marten Rebane <marten.rebane@nortal.com>
2021-02-26 19:09:33 +0200

    Improved focus order
    * MOPPAND-565
    
    Signed-off-by: Marten Rebane marten.rebane@nortal.com

--- app/src/main/java/ee/ria/DigiDoc/android/main/settings/SettingsView.java
+++ app/src/main/java/ee/ria/DigiDoc/android/main/settings/SettingsView.java
@@ -1,9 +1,13 @@
 package ee.ria.DigiDoc.android.main.settings;
 
 import android.content.Context;
-import androidx.coordinatorlayout.widget.CoordinatorLayout;
-import androidx.appcompat.widget.Toolbar;
 import android.util.AttributeSet;
+import android.view.View;
+import android.view.accessibility.AccessibilityEvent;
+import android.widget.TextView;
+
+import androidx.appcompat.widget.Toolbar;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
 
 import ee.ria.DigiDoc.R;
 import ee.ria.DigiDoc.android.Application;
@@ -16,6 +20,7 @@ import static com.jakewharton.rxbinding2.support.v7.widget.RxToolbar.navigationC
 public final class SettingsView extends CoordinatorLayout {
 
     private final Toolbar toolbarView;
+    private final TextView toolbarTitleView;
 
     private final Navigator navigator;
 
@@ -33,8 +38,28 @@ public final class SettingsView extends CoordinatorLayout {
         super(context, attrs, defStyleAttr);
         inflate(context, R.layout.main_settings, this);
         toolbarView = findViewById(R.id.toolbar);
+        toolbarTitleView = getToolbarViewTitle();
         navigator = Application.component(context).navigator();
         disposables = new ViewDisposables();
+
+        if (toolbarTitleView != null) {
+            toolbarTitleView.setContentDescription("\u202F");
+            toolbarTitleView.postDelayed(() -> {
+                toolbarTitleView.requestFocus();
+                toolbarTitleView.sendAccessibilityEvent(AccessibilityEvent.TYPE_VIEW_FOCUSED);
+            }, 2500);
+        }
+    }
+
+    private TextView getToolbarViewTitle() {
+        for (int i = 0; i < toolbarView.getChildCount(); i++) {
+            View childView = toolbarView.getChildAt(i);
+            if (childView instanceof TextView) {
+                return (TextView) childView;
+            }
+        }
+
+        return null;
     }
 
     @Override
