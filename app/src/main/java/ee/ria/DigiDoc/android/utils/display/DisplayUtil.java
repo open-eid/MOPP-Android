cdfe6aad162ab0569e0c7e3164037a1ab72489cb
Marten Rebane <54431068+martenrebane@users.noreply.github.com>
2022-03-03 17:50:20 +0200

    Error dialog screen width fixes
    * MOPPAND-652
    
    Signed-off-by: Marten Rebane marten.rebane@nortal.com

--- app/src/main/java/ee/ria/DigiDoc/android/utils/display/DisplayUtil.java
+++ app/src/main/java/ee/ria/DigiDoc/android/utils/display/DisplayUtil.java
@@ -1,9 +1,16 @@
 package ee.ria.DigiDoc.android.utils.display;
 
+import static android.view.ViewGroup.LayoutParams.WRAP_CONTENT;
+
+import android.content.Context;
+import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.util.DisplayMetrics;
 
 public class DisplayUtil {
+
+    private static final double PORTRAIT_WIDTH_MULTIPLIER = 0.9;
+    private static final double LANDSCAPE_WIDTH_MULTIPLIER = 0.8;
 
     public static int getDisplayMetricsDpToInt(Resources resources, int px) {
         final float scale = resources.getDisplayMetrics().density;
@@ -14,7 +21,27 @@ public class DisplayUtil {
         return (int) (px * displayMetrics.density + 0.5f);
     }
 
+    public static int getDeviceOrientation(Context context) {
+        return context.getResources().getConfiguration().orientation;
+    }
+
     public static int getDeviceWidth() {
         return Resources.getSystem().getDisplayMetrics().widthPixels;
+    }
+
+    public static int getDialogPortraitWidth() {
+        return (int) (getDeviceWidth() * PORTRAIT_WIDTH_MULTIPLIER);
+    }
+
+    public static int getDialogLandscapeWidth() {
+        return (int) (getDeviceWidth() * LANDSCAPE_WIDTH_MULTIPLIER);
+    }
+
+    public static int getDeviceLayoutWidth(Context context) {
+        if (getDeviceOrientation(context) == Configuration.ORIENTATION_PORTRAIT) {
+            return getDialogPortraitWidth();
+        } else {
+            return getDialogLandscapeWidth();
+        }
     }
 }
