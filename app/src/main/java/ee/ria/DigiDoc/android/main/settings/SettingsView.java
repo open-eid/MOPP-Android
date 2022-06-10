8681e9c09579cb385eac74e477b128d232f2685d
Marten Rebane <54431068+martenrebane@users.noreply.github.com>
2022-06-10 14:37:14 +0300

    Open all types setting only icon clickable (#269)
    * MOPPAND-764

--- app/src/main/java/ee/ria/DigiDoc/android/main/settings/SettingsView.java
+++ app/src/main/java/ee/ria/DigiDoc/android/main/settings/SettingsView.java
@@ -1,14 +1,19 @@
 package ee.ria.DigiDoc.android.main.settings;
 
+import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toolbar;
 
+import androidx.appcompat.widget.SwitchCompat;
 import androidx.coordinatorlayout.widget.CoordinatorLayout;
 
 import ee.ria.DigiDoc.R;
+import ee.ria.DigiDoc.android.Activity;
 import ee.ria.DigiDoc.android.Application;
 import ee.ria.DigiDoc.android.utils.ViewDisposables;
 import ee.ria.DigiDoc.android.utils.navigator.Navigator;
@@ -48,6 +53,17 @@ public final class SettingsView extends CoordinatorLayout {
         if (toolbarTitleView != null) {
             toolbarTitleView.setContentDescription("\u202F");
         }
+
+        Activity activityContext = (Activity) this.getContext();
+        SwitchCompat openAllFileTypesSwitch = findViewById(R.id.mainSettingsOpenAllFileTypes);
+        if (openAllFileTypesSwitch != null && activityContext != null) {
+            openAllFileTypesSwitch.setChecked(activityContext.getSettingsDataStore().getIsOpenAllFileTypesEnabled());
+
+            openAllFileTypesSwitch.setOnCheckedChangeListener((buttonView, isChecked) -> {
+                activityContext.getSettingsDataStore().setIsOpenAllFileTypesEnabled(isChecked);
+                restartIntent();
+            });
+        }
     }
 
     private TextView getToolbarViewTitle() {
@@ -59,6 +75,15 @@ public final class SettingsView extends CoordinatorLayout {
         }
 
         return null;
+    }
+
+    private void restartIntent() {
+        PackageManager packageManager = getContext().getPackageManager();
+        Intent intent = packageManager.getLaunchIntentForPackage(getContext().getPackageName());
+        ComponentName componentName = intent.getComponent();
+        Intent restartIntent = Intent.makeRestartActivityTask(componentName);
+        restartIntent.setAction(Intent.ACTION_CONFIGURATION_CHANGED);
+        getContext().startActivity(restartIntent);
     }
 
     @Override
