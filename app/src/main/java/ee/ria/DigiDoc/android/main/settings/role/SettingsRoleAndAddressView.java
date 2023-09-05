2414f3691c2547fc7255159f0f4a7d492766ade9
Marten Rebane <54431068+martenrebane@users.noreply.github.com>
2023-09-05 19:43:06 +0300

    Implemented new Settings design

--- app/src/main/java/ee/ria/DigiDoc/android/main/settings/role/SettingsRoleAndAddressView.java
+++ app/src/main/java/ee/ria/DigiDoc/android/main/settings/role/SettingsRoleAndAddressView.java
@@ -1,65 +1,67 @@
-package ee.ria.DigiDoc.android.main.settings;
+package ee.ria.DigiDoc.android.main.settings.role;
 
-import static com.jakewharton.rxbinding4.view.RxView.clicks;
+import static com.jakewharton.rxbinding4.widget.RxCompoundButton.checkedChanges;
 import static com.jakewharton.rxbinding4.widget.RxToolbar.navigationClicks;
 import static ee.ria.DigiDoc.android.main.settings.util.SettingsUtil.getToolbarViewTitle;
 
 import android.content.Context;
 import android.util.AttributeSet;
-import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toolbar;
 
+import androidx.appcompat.widget.SwitchCompat;
 import androidx.coordinatorlayout.widget.CoordinatorLayout;
 
 import ee.ria.DigiDoc.R;
+import ee.ria.DigiDoc.android.Activity;
 import ee.ria.DigiDoc.android.Application;
-import ee.ria.DigiDoc.android.main.settings.access.SettingsAccessScreen;
-import ee.ria.DigiDoc.android.main.settings.role.SettingsRoleAndAddressScreen;
+import ee.ria.DigiDoc.android.main.settings.SettingsDataStore;
 import ee.ria.DigiDoc.android.utils.ViewDisposables;
 import ee.ria.DigiDoc.android.utils.navigator.Navigator;
 import ee.ria.DigiDoc.android.utils.navigator.Transaction;
-import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
-import io.reactivex.rxjava3.core.Observable;
-import io.reactivex.rxjava3.disposables.Disposable;
-import kotlin.Unit;
 
-public final class SettingsView extends CoordinatorLayout {
+public final class SettingsRoleAndAddressView extends CoordinatorLayout {
 
     private final Toolbar toolbarView;
+    private final SwitchCompat askRoleAndAddressSwitch;
 
     private final Navigator navigator;
-
+    private final SettingsDataStore settingsDataStore;
     private final ViewDisposables disposables;
 
-    private final Button accessCategory;
-    private final Button roleAndAddressCategory;
-
-    public SettingsView(Context context) {
+    public SettingsRoleAndAddressView(Context context) {
         this(context, null);
     }
 
-    public SettingsView(Context context, AttributeSet attrs) {
+    public SettingsRoleAndAddressView(Context context, AttributeSet attrs) {
         this(context, attrs, 0);
     }
 
-    public SettingsView(Context context, AttributeSet attrs, int defStyleAttr) {
+    public SettingsRoleAndAddressView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        inflate(context, R.layout.main_settings, this);
+        inflate(context, R.layout.main_settings_role_and_address, this);
         toolbarView = findViewById(R.id.toolbar);
         TextView toolbarTitleView = getToolbarViewTitle(toolbarView);
         navigator = Application.component(context).navigator();
+        settingsDataStore = Application.component(context).settingsDataStore();
         disposables = new ViewDisposables();
 
-        accessCategory = findViewById(R.id.mainSettingsAccessCategory);
-        roleAndAddressCategory = findViewById(R.id.mainSettingsRoleAndAddressCategory);
-
-        toolbarView.setTitle(R.string.main_settings_title);
+        toolbarView.setTitle(R.string.main_settings_role_and_address_button);
         toolbarView.setNavigationIcon(androidx.appcompat.R.drawable.abc_ic_ab_back_material);
         toolbarView.setNavigationContentDescription(R.string.back);
 
         if (toolbarTitleView != null) {
             toolbarTitleView.setContentDescription("\u202F");
+        }
+
+        Activity activityContext = (Activity) this.getContext();
+
+        askRoleAndAddressSwitch = findViewById(R.id.mainSettingsAskRoleAndAddress);
+        if (askRoleAndAddressSwitch != null && activityContext != null) {
+            askRoleAndAddressSwitch.setChecked(activityContext.getSettingsDataStore().getIsRoleAskingEnabled());
+
+            askRoleAndAddressSwitch.setOnCheckedChangeListener((buttonView, isChecked) ->
+                    activityContext.getSettingsDataStore().setIsRoleAskingEnabled(isChecked));
         }
     }
 
@@ -69,12 +71,8 @@ public final class SettingsView extends CoordinatorLayout {
         disposables.attach();
         disposables.add(navigationClicks(toolbarView).subscribe(o ->
                 navigator.execute(Transaction.pop())));
-        disposables.add(clicks(accessCategory).subscribe(o ->
-                navigator.execute(
-                        Transaction.push(SettingsAccessScreen.create()))));
-        disposables.add(clicks(roleAndAddressCategory).subscribe(o ->
-                navigator.execute(
-                        Transaction.push(SettingsRoleAndAddressScreen.create()))));
+        disposables.add(checkedChanges(askRoleAndAddressSwitch)
+                .subscribe(settingsDataStore::setIsRoleAskingEnabled));
     }
 
     @Override
