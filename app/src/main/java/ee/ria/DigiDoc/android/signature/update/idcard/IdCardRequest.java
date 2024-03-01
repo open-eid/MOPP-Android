5d96bd633d0298fbe8a633528bde05731b4850fd
Marten Rebane <54431068+martenrebane@users.noreply.github.com>
2024-03-01 14:43:14 +0200

    Updated ACS library, fixed PIN in memory

--- app/src/main/java/ee/ria/DigiDoc/android/signature/update/idcard/IdCardRequest.java
+++ app/src/main/java/ee/ria/DigiDoc/android/signature/update/idcard/IdCardRequest.java
@@ -1,18 +1,52 @@
 package ee.ria.DigiDoc.android.signature.update.idcard;
 
-import com.google.auto.value.AutoValue;
-
 import ee.ria.DigiDoc.android.signature.update.SignatureAddRequest;
 import ee.ria.DigiDoc.idcard.Token;
 
-@AutoValue
 public abstract class IdCardRequest implements SignatureAddRequest {
 
-    public abstract Token token();
+    private final Token token;
+    private final byte[] pin2;
 
-    public abstract String pin2();
+    protected IdCardRequest(Token token, byte[] pin2) {
+        this.token = token;
+        this.pin2 = pin2;
+    }
 
-    static IdCardRequest create(Token token, String pin2) {
-        return new AutoValue_IdCardRequest(token, pin2);
+    public Token token() {
+        return token;
+    }
+
+    public byte[] pin2() {
+        return pin2;
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static class Builder {
+        private Token token;
+        private byte[] pin2;
+
+        public Builder token(Token token) {
+            this.token = token;
+            return this;
+        }
+
+        public Builder pin2(byte[] pin2) {
+            this.pin2 = pin2;
+            return this;
+        }
+
+        public IdCardRequest build() {
+            if (token == null) {
+                throw new IllegalStateException("Token must be set");
+            }
+            if (pin2 == null) {
+                throw new IllegalStateException("PIN2 must be set");
+            }
+            return new IdCardRequest(token, pin2) {};
+        }
     }
 }
